e5427aa28371d9fc968fd282cf7f069f858995cc
Dennis Turco <dennisturco@gmail.com>
2025-09-16 22:57:03 +0200

    matematica final resurrection

--- src/data/sidebarMatematica.ts
+++ src/data/sidebarMatematica.ts
@@ -5,4 +5,30 @@ export const sidebar = {
     { title: "Statistica", url: "/matematica/numeri/3_statistica" },
     { title: "Potenze", url: "/matematica/numeri/4_potenze" },
   ],
+  "Polinomi": [
+    { title: "Ruffini", url: "/matematica/polinomi/1_ruffini" },
+    { title: "Divisione Polinomiale", url: "/matematica/polinomi/2_divisione-polinomiale" },
+  ],
+  "Equazioni": [
+    { title: "Equazioni di Primo Grado", url: "/matematica/equazioni/1_equazioni-di-primo-grado" },
+  ],
+  "Logaritmi": [
+    { title: "Logaritmi", url: "/matematica/logaritmi/1_logaritmi" },
+  ],
+  "Limiti e Teoremi ": [
+    { title: "Limiti", url: "/matematica/limiti/1_limiti" },
+    { title: "Teorema di Esistenza Degli Zeri", url: "/matematica/limiti/2_teorema-di-esistenza-degli-zeri" },
+    { title: "Teorema di Weistrass", url: "/matematica/limiti/3_teorema-di-weistrass" },
+  ],
+  "Derivate": [
+    { title: "Derivate", url: "/matematica/derivate/1_derivate" },
+  ],
+  "Altro": [
+    { title: "Parabola", url: "/matematica/altro/1_parabola" },
+    { title: "Ellisse", url: "/matematica/altro/2_ellisse" },
+    { title: "Iperbole", url: "/matematica/altro/3_iperbole" },
+    { title: "Goniometria", url: "/matematica/altro/4_goniometria" },
+    { title: "Trigonometria", url: "/matematica/altro/5_trigonometria" },
+    { title: "Studio di Funzione", url: "/matematica/altro/6_studio-di-funzione" },
+  ],
 };
